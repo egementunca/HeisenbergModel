f687ae6703602846d89068167b9e8bb6853da2f6
egementunca <egementunca@gmail.com>
2024-04-05 15:04:46 +0300

    ready for cluster

--- scripts/phaseDiagram_deltavsTc_nematic.py
+++ scripts/phaseDiagram_deltavsTc_nematic.py
@@ -17,7 +17,7 @@ def checkPhase(g, p, n, q, dim, pool_size, l_prec, rg_step, iter_num):
 	for i in range(iter_num):
 		jNew = (jNematic + jFerro)/2
 		f = flow.rgTrajectoryVacancy(J=jNew, g=g, p=p, q=q, n=n, dim=dim, pool_size=pool_size, l_prec=l_prec, rg_step=rg_step)
-		check_pool = normalize_pool(fOrder[-1])
+		check_pool = normalize_pool(f[-1])
 		avg_lfc = np.mean(abs(check_pool), axis=0)
 		if (sum(avg_lfc) < 15.37):
 			jNematic = jNew
@@ -37,7 +37,8 @@ def phase_diagram_deltavsTc(p, q, n, dim, pool_size, l_prec, rg_step, iter_num):
 	for i,g in enumerate(g_vals):
 		j_crit = checkPhase(g=g, p=p, n=n, q=q, dim=dim, pool_size=pool_size, l_prec=l_prec, rg_step=rg_step, iter_num=iter_num)[-1][0]
 		j_vals[i] = j_crit
-	np.save('deltavsTc_p{}_nematic.npy'.format(p), (g_vals, j_vals))
+	np.save('./new_data/deltavsTc_p{}_nematic.npy'.format(p), (g_vals, j_vals))
 	return 0
 
 phase_diagram_deltavsTc(p=0.1, q=0, n=9, dim=3, pool_size=30000, l_prec=21, rg_step=30, iter_num=30)
+#phase_diagram_deltavsTc(p=0.5, q=0, n=9, dim=3, pool_size=30000, l_prec=21, rg_step=30, iter_num=30)
